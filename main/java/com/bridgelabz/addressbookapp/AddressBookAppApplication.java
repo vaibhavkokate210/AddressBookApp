9ee9fff583dba9b020484665b2ee679ec9392d81
Vaibhav Kokate <vaibhav.kokate@morningstar.com>
2021-10-01 02:00:15 +0530

    [Vaibhav] added use LomBok library for logging

--- main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
+++ main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -3,11 +3,15 @@ package com.bridgelabz.addressbookapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddressBookAppApplication.class, args);
+		log.info("Address Book App started");
 	}
 
 }
--- main/java/com/bridgelabz/addressbookapp/model/ResponseDTO.java
+++ main/java/com/bridgelabz/addressbookapp/model/ResponseDTO.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.addressbookapp.model;
 
-public class ResponseDTO 
+import lombok.Data;
+
+public @Data class ResponseDTO 
 {
 	private String message;
 	private Object data;
@@ -8,18 +10,6 @@ public class ResponseDTO
 	public ResponseDTO(String message, Object data) {
 		super();
 		this.message = message;
-		this.data = data;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public Object getData() {
-		return data;
-	}
-	public void setData(Object data) {
 		this.data = data;
 	}
 }
