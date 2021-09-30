16fc1cac3b80a22e861c1bf55611f74473927473
Vaibhav Kokate <vaibhav.kokate@morningstar.com>
2021-10-01 02:09:46 +0530

    [Vaibhav] added determine loggin levels,logging to console or file etc

--- main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
+++ main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -2,6 +2,7 @@ package com.bridgelabz.addressbookapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,8 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookAppApplication.class, args);
-		log.info("Address Book App started");
+		ApplicationContext context=SpringApplication.run(AddressBookAppApplication.class, args);
+		log.info("Employee payroll App started in {} Environment",context.getEnvironment().getProperty("environment"));
 	}
 
 }
